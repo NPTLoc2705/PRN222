b8dde6e9a802fbb19b44d8fbdc6efc129046ed40
Vualidon123 <lacduy19@gmail.com>
2025-07-06 07:17:39 +0700

    asd

---
+++ mvc.app/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc.app.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Dashboard()
+        {
+            var role = HttpContext.Session.GetString("Role");            // This action could be used to display admin dashboard information
+          /*  if(role != "Admin")
+            {
+                // If the user is not an admin, redirect to the home page or an error page
+                return RedirectToAction("Index", "Home");
+            }*/
+            return View("AdminPage");
+        }
+
+
+    }
+}
