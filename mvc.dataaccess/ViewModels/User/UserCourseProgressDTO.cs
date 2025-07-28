2bdd233dd597383c4e7106bbe7ee3fc6b086eccd
nrjbutsecond <giathinh890355@gmail.com>
2025-07-28 13:46:55 +0700

    fix user profile and lesson index UI

---
+++ mvc.dataaccess/ViewModels/User/UserCourseProgressDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvc.dataaccess.ViewModels.User
+{
+    public class UserCourseProgressDTO
+    {
+        public Guid CourseId { get; set; }
+        public string CourseTitle { get; set; }   
+        public Guid? LessonId { get; set; }
+        public string LessonTitle { get; set; }   
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public DateTime LastAccessed { get; set; }
+        public decimal ProgressPercentage { get; set; }
+    }
+}
